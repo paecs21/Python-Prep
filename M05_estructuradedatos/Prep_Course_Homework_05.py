059c012f803ae85c462ffdf86c3ca010f2c9e95a
ACisnerosSoria <paecs.21@gmail.com>
2023-09-13 19:29:46 -0300

    homework_estructura de datos

--- M05_estructuradedatos/Prep_Course_Homework_05.py
+++ M05_estructuradedatos/Prep_Course_Homework_05.py
@@ -7,21 +7,22 @@
 
 # In[3]:
 
-
+ciudades=['Lima','Buenos Aires','Bogota','Madrid','Barcelona','La Paz']
+print(ciudades)
 
 
 # 2) Imprimir por pantalla el segundo elemento de la lista
 
 # In[4]:
 
-
+print(ciudades[1])
 
 
 # 3) Imprimir por pantalla del segundo al cuarto elemento
 
 # In[8]:
 
-
+print(ciudades[1:4])
 
 
 
@@ -29,7 +30,7 @@
 
 # In[12]:
 
-
+print(type(ciudades))
 
 
 
@@ -37,7 +38,7 @@
 
 # In[14]:
 
-
+print(ciudades[2:])
 
 
 
@@ -45,7 +46,7 @@
 
 # In[15]:
 
-
+print(ciudades[:4])
 
     
 
@@ -54,7 +55,9 @@
 
 # In[16]:
 
-
+ciudades.append('Lima')
+ciudades.append('Cordoba')
+print(ciudades)
 
 
 
@@ -66,7 +69,8 @@
 
 # In[20]:
 
-
+ciudades.insert(3,'Montevideo')
+print(ciudades)
 
 
 
@@ -78,7 +82,9 @@
 # 9) Concatenar otra lista a la ya creada
 
 # In[22]:
-
+aux={1,2,3}
+ciudades.extend(aux)
+print(ciudades)
 
 
 
@@ -86,7 +92,8 @@
 
 # In[23]:
 
-
+print(ciudades.index('Lima'))
+#Ddevuelve el indice de la primera aparición del valor y no del agregado en el punto 7
 
 
 
@@ -94,14 +101,15 @@
 
 # In[24]:
 
-
+print(ciudades.index('Brazilia'))
 
 
 
 # 12) Eliminar un elemento de la lista
 
 # In[25]:
-
+ciudades.remove('Lima')
+print(ciudades)
 
 
 
@@ -109,7 +117,8 @@
 # 13) ¿Qué pasa si el elemento a eliminar no existe?
 
 # In[27]:
-
+ciudades.remove('Paris')
+print(ciudades)
 
 
 
@@ -117,7 +126,9 @@
 # 14) Extraer el úlimo elemento de la lista, guardarlo en una variable e imprimirlo
 
 # In[28]:
-
+tacho=ciudades.pop()
+print(ciudades)
+print(tacho)
 
 
 
@@ -126,21 +137,22 @@
 
 # In[29]:
 
-
+print(ciudades*4)
 
 
 # 16) Crear una tupla que contenga los números enteros del 1 al 20
 
 # In[32]:
 
-
+mi_tupla=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+print(mi_tupla)
 
 
 # 17) Imprimir desde el índice 10 al 15 de la tupla
 
 # In[35]:
 
-
+print(mi_tupla[10:15])
 
 
 # 18) Evaluar si los números 20 y 30 están dentro de la tupla
@@ -148,13 +160,18 @@
 # In[41]:
 
 
-
-
+print(20 in  mi_tupla)
+print(30 in  mi_tupla)
 
 # 19) Con la lista creada en el punto 1, validar la existencia del elemento 'París' y si no existe, agregarlo. Utilizar una variable e informar lo sucedido.
 
 # In[48]:
-
+res = 'París'
+if (not(res in ciudades)):
+    ciudades.append(res)
+    print('Se insertó el elemento', res)
+else:
+    print('El elemento', res, 'ya existía')
 
 
 
@@ -163,7 +180,9 @@
 
 # In[51]:
 
-
+print(ciudades)
+print(ciudades.count('Lima'))
+print(mi_tupla.count(5))
 
 
 
@@ -171,14 +190,17 @@
 
 # In[52]:
 
-
-
+aux=list(mi_tupla)
+print(aux)
 
 
 # 22) Desempaquetar solo los primeros 3 elementos de la tupla en 3 variables
 
 # In[55]:
-
+a,b,c,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_=mi_tupla
+print(a)
+print(b)
+print(c)
 
 
 
@@ -187,7 +209,9 @@
 
 # In[57]:
 
-
+mi_dicc = {  'Ciudad': ciudades, 
+'País': ['Brasil','Paraguay','Ecuador','Uruguay','Chile','Perú','Venezuela','Colombia','Méjico','Uruguay','España','Italia','Francia'], 
+'Continente' : ['América','América','América','América','América','América','América','América','América','América','Europa','Europa','Europa']}
 
 
 
@@ -195,7 +219,7 @@
 # 24) Imprimir las claves del diccionario
 
 # In[59]:
-
+print(mi_dicc.keys())
 
 
 
@@ -203,6 +227,6 @@
 
 # In[61]:
 
-
+print(mi_dicc['Ciudad'])
 
 
